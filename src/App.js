0f342b8b85f518398efab97ea1f32da09be358ed
betul <bsenger@aof.anadolu.edu.tr>
2023-04-02 17:18:52 +0300

    app1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
 
 function App() {
+  const [pencereBoyutu, pencereBoyutuGuncelle] = useState({
+    genislik: window.innerWidth,
+    yukseklik: window.innerHeight,
+  });
+
+  useEffect(() => {
+    function boyutlandirmaKodu() {
+      pencereBoyutuGuncelle({
+        genislik: window.innerWidth,
+        yukseklik: window.innerHeight,
+      });
+    }
+    window.addEventListener("resize", boyutlandirmaKodu);
+    return () => window.removeEventListener("resize", boyutlandirmaKodu);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <div>
+      <p>Pencere genişliği: {pencereBoyutu.genislik}</p>
+      <p>Pencere yüksekliği: {pencereBoyutu.yukseklik}</p>
     </div>
   );
 }
