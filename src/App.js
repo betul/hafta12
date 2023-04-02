ae0bb63b7fced6af4c510a826a21fb3e4fabf42c
betul <bsenger@aof.anadolu.edu.tr>
2023-04-02 20:42:00 +0300

    Use effect 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
 
 function App() {
+  const [sayac, sayacGuncelle] = useState(0);
+  const [ad, adGuncelle] = useState("Ad tanımsız");
+  
+  useEffect(() => {
+    document.title = `${sayac} - ${ad}`;
+  }, [sayac, ad]);
+  
+
+  function adFonksiyonu() {
+    let zaman = new Date().getSeconds();
+    adGuncelle("Ahmet Yılmaz: " + zaman);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <>
+      <p>{sayac}</p>
+      <button onClick={() => sayacGuncelle((oncekiDeger) => oncekiDeger + 1)}>
+        Arttır
+      </button>
+
+      <p>Ad: {ad}</p>
+      <button onClick={adFonksiyonu}>Ad Güncelle</button>
+    </>
   );
 }
 
